98f334621f7db15c1afa4278df702c5c7432bafd
Lernspiele-Pferd <woehrlek@gmx.de>
2015-05-26 14:01:43 +0200

    Zurück-Button angepasst

--- lernspiele/Dickdarm-Teil-1/spielEinstellungen.js
+++ lernspiele/Dickdarm-Teil-1/spielEinstellungen.js
@@ -48,7 +48,7 @@ function nach_Spieleinstellungen_fragen()
 
     output_fenster.document.write ('<br><input type="button" name="button_SpielStarten" value="Starte das Lernspiel zu Thema 4: Dickdarm Teil 1" onClick="top.starte_spiel();"><br>\n');
     output_fenster.document.write ('<br><input type="button" name="button_BilderZeigen" value="Zeige alle Fragen und Antworten" onClick="top.zeige_bilder();"><br>\n');
-    output_fenster.document.write ('<br><input type="button" name="button_Zurueck" value="Zurück zur Startseite" onClick="history.back()"><br>\n');
+    output_fenster.document.write ('<br><a href="http://lernspiele-pferd.github.io/">Zurück zur Startseite</a><br>\n');
    
     output_fenster.document.write ("</form>\n");
  } 
