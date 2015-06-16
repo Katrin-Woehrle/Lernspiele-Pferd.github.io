3a8e7aeef9805a2550401017a4d135e4db958715
Lernspiele-Pferd <woehrlek@gmx.de>
2015-06-16 22:00:02 +0200

    Create bildernamen.js

---
+++ lernspiele/einleitung/bildernamen.js
@@ -0,0 +1,12 @@
+var zeige_bild_name = false;
+
+var max_zusammenpassende_bilder = 2;
+
+var verdecktes_bild_name = "verdeckt.png";   
+
+var vordefinierte_bilder_namen = new Array();
+
+i = 0;
+
+vordefinierte_bilder_namen[i++] = "einleitung_frage_1.png";
+vordefinierte_bilder_namen[i++] = "einleitung_antwort_1.png";
