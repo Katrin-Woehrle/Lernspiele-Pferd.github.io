5e5007dec1ebf4e9b78f06bd565e0184deb7cc0f
Lernspiele-Pferd <woehrlek@gmx.de>
2015-11-02 23:45:00 +0100

    Create bildhoehe.js

---
+++ bildhoehe.js
@@ -0,0 +1,33 @@
+function getBrowser()
+   	{
+		if (navigator.appName == "Netscape")
+			return "FF";
+		if (navigator.appName == "Microsoft Internet Explorer")
+			return "IE";
+    }
+
+function getClientWidth()
+	{
+	    var browser = getBrowser();
+	    	if (browser == "IE")
+	    		{
+				    return window.document.body.clientWidth;
+				}
+		    if (browser == "FF")
+		    	{
+					return window.innerWidth;
+				}
+	}
+
+function getClientHeight()
+	{
+	    var browser = getBrowser();
+	    	if (browser == "IE")
+	    		{
+			    	return window.document.body.clientHeight;
+				}
+			if (browser == "FF")
+				{
+					return window.innerHeight;
+				}
+	}
